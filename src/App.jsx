be3bf77f370f6dbb37f0c83d14fcd2ba90d9c9e9
Vishal Yadav <yadavvishal4065@gmail.com>
2024-09-26 18:32:43 +0530

    Making Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import { useContext } from "react";
 import Main from "./components/Mainpage/Main";
-import useScreenWidth from "./components/CustomComponent/ScreenWidth";
-import AccessDenied from "./components/AccessDenied/AccessDenied";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  const screenWidth = useScreenWidth();
-
-  if (screenWidth < 1200) {
-    // Adjust the threshold as needed
-    return <AccessDenied />;
-  }
   return (
     <>
       <Sidebar />
